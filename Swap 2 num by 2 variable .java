bb791b86b69e8d8e8fcda1193c8a5f6609b2bf1e
Yogeshsonar22 <sonaryogesh512@gmail.com>
2025-07-30 04:33:57 +0530

    Swapping number
    4 th code

---
+++ Swap 2 num by 2 variable .java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+    public static void main(String[] args) throws java.lang.Exception
+    {
+
+     
+     int a=4; int b=1;
+        System.out.println("Swap 2 number by 2 variable");
+        System.out.println("A: "+ a + " B: "+b);
+        a= a+b;  //5
+        b= a-b; // 4
+        a= a-b;  //1
+        System.out.println("A: "+ a + " B: "+b);
+      
+    }
+}
+
