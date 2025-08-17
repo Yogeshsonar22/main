f5c8557cf757cb9a4adfbb8fec53f7b768a4c841
Yogesh sonar <sonaryogesh512@gmail.com>
2025-08-17 16:31:30 +0530

    Sort first half in ascending order and second half in descending in array
    Sort first half in ascending order and second half in descending

---
+++ Sort first half in ascending order and second half in descending .java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int arr[]={1,2,3,4,5,6,7,8,9,10};
+		int swap=0;
+		for (int i=0;i<arr.length/2 ;i++ ){
+		    System.out.print(arr[i]);
+		    System.out.print(" ");
+		}
+		
+		for (int i=arr.length-1;i>(arr.length-1)/2 ; i--) {
+		    System.out.print(arr[i]+" ");
+	}
+
+
+
+	}
+}
